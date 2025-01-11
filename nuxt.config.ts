f42d66169eb4ac518ed570ced43f80d77b84d388
Rnd00m <39692856+Rnd00m@users.noreply.github.com>
2025-01-11 16:10:27 +0100

    chore: add tailwind and fonts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,29 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import Aura from '@primevue/themes/aura';
+
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: {enabled: true},
+  css: ['@/assets/scss/main.scss'],
+  modules: [
+    '@primevue/nuxt-module'
+  ],
+  primevue: {
+    options: {
+      theme: {
+        preset: Aura,
+        options: {
+          cssLayer: {
+            name: 'primevue',
+            order: 'tailwind-base, primevue, tailwind-utilities'
+          }
+        }
+      }
+    }
+  },
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
 })
