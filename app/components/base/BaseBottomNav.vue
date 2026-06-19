88d73e284f51bb2355c2dd8ffe225be94d565312
Rnd00m <39692856+Rnd00m@users.noreply.github.com>
2026-06-19 13:03:34 +0200

    fix: missing BaseBottomNav

---
+++ app/components/base/BaseBottomNav.vue
@@ -0,0 +1,22 @@
+<template>
+  <nav class="bottom-nav">
+    <RouterLink v-for="item in items" :key="item.route" v-slot="{ href, navigate, isActive }" :to="item.route" custom>
+      <a
+        v-ripple
+        :href="href"
+        :aria-label="item.label"
+        class="bottom-nav-link"
+        :class="{ 'route-active': isActive }"
+        @click="navigate"
+      >
+        <span :class="item.icon" class="bottom-nav-icon"/>
+      </a>
+    </RouterLink>
+  </nav>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  items: { icon: string; label: string; route: string }[];
+}>();
+</script>
